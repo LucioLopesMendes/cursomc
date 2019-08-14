823f218c3255e6aea39075f9f45f3ce11b9c4917
Lucio Lopes Mendes <luciolopesmendes@gmail.com>
2019-08-14 07:46:31 -0300

    Projeto Criado

---
+++ src/main/java/com/lucio/cursomc/apirest/CursomcApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.lucio.cursomc.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucio/cursomc/apirest/CursomcApiRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lucio.cursomc.apirest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursomcApiRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
